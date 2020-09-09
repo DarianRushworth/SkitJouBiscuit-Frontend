0cd5c696d2fada3380b3dfdbb3d2e55cc9acdfac
DarianRushworth <darianerrolrushworth@gmail.com>
2020-09-09 14:07:08 +0200

    React-Native setup amd HomeScreen Skeleton complete

---
+++ StyledComponents/homeScreen.js
@@ -0,0 +1,28 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        flexDirection: "column",
+    },
+    image: {
+        flex: 1,
+        resizeMode: "cover",
+        justifyContent: "center",
+    },
+    textContainer : {
+        flex: 2,
+        flexDirection: "column"
+    },
+    text: {
+        color: "yellow",
+        textAlign: "center",
+        fontSize: 30,
+        fontWeight: "bold",
+    },
+    buttonContainer : {
+        flex: 0,
+        flexDirection: "column",
+        justifyContent: "center",
+    }
+})
