79651a5afe7ca8be535120d96340347bd70728ed
DarianRushworth <darianerrolrushworth@gmail.com>
2020-09-21 12:10:12 +0200

    Validation functions created, inputted in form

--- helpers/Validations.js
+++ helpers/Validations.js
@@ -9,3 +9,41 @@ export const lengthValidator = (data) => {
         return "Password can contain anything, but must be 6 characters or more."
     }
 }
+
+export const validateField = (validators, value) => {
+    let error = ""
+
+    validators.forEach((validator) => {
+      const validationError = validator(value)
+      if (validationError) {
+        error = validationError
+      }
+
+    })
+    return error
+}
+
+export const validateFields = (fields, values) => {
+    const errors = {}
+    const fieldKeys = Object.keys(fields)
+
+    fieldKeys.forEach((key) => {
+      const field = fields[key]
+      const validators = field.validators
+      const value = values[key]
+
+      if (validators && validators.length > 0) {
+        const error = validateField(validators, value)
+  
+        if (error) {
+          errors[key] = error
+        }
+      }
+    })
+  
+    return errors
+}
+
+export const hasValidationError = (errors) => {
+    return Object.values(errors).find((error) => error.length > 0)
+}
