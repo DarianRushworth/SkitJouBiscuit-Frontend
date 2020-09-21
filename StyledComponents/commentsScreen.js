c6cb8470a80fb34b4613518975d8af5a832f03a5
DarianRushworth <darianerrolrushworth@gmail.com>
2020-09-21 20:43:40 +0200

    Input for new comments

---
+++ StyledComponents/commentsScreen.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+    card:{
+        backgroundColor: "tomato",
+    },
+    text: {
+        color: "white",
+        textAlign: "left"
+    },
+    input: {
+        flex: 1,
+    }
+})
--- components/CommentsScreen.js
+++ components/CommentsScreen.js
@@ -1,38 +1,61 @@
 import React from "react"
-import { Text, View } from "react-native"
+import { Text, View, ScrollView } from "react-native"
 import { useSelector } from "react-redux"
-import { Card } from "react-native-elements"
+import { Card, Input } from "react-native-elements"
+import Icon from "react-native-vector-icons/FontAwesome"
 
 import { selectComments } from "../store/party/selectors"
+import { styles } from "../StyledComponents/commentsScreen"
 
 export default function CommentsScreen(){
     const comments = useSelector(selectComments)
-    console.log("comments from state", comments)
+    
+    const custom = styles
 
-    if(comments.length === 0){
-    return (
+    const display = () => {
+        if(comments.length === 0){
+        return (
         <View>
             <Text>
                 Comments here!
             </Text>
         </View>
-    )
-    } else {
+        )
+        } else {
         return comments.map(comment => {
             return (
                 <Card
-                    key={comment.id}>
-                    <Card.Title>
+                    key={comment.id}
+                    containerStyle={custom.card}>
+                    <Card.Title
+                        style={custom.text}>
                         {comment.user.fullName}
                     </Card.Title>
                     <Card.Divider />
                     <View>
-                        <Text>
+                        <Text
+                            style={custom.text}>
                             {comment.input}
                         </Text>
                     </View>
                 </Card>
             )
-        })
+            })
+        }
     }
+
+    return (
+        <View
+            style={custom.input}>
+            <ScrollView>
+                {display()}
+            </ScrollView>
+            <View>
+            <Input
+                placeholder="Comment"
+                leftIcon={{ type: 'font-awesome', name: 'comment' }}
+                />
+            </View>
+        </View>
+    )
 }
