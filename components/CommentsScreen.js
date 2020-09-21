9bd076ecf13f1860524013ea5543e38495f1e7f2
DarianRushworth <darianerrolrushworth@gmail.com>
2020-09-21 18:11:47 +0200

    Comments displayed, no styling as of yet

--- components/CommentsScreen.js
+++ components/CommentsScreen.js
@@ -1,13 +1,15 @@
 import React from "react"
 import { Text, View } from "react-native"
 import { useSelector } from "react-redux"
+import { Card } from "react-native-elements"
 
 import { selectComments } from "../store/party/selectors"
 
 export default function CommentsScreen(){
     const comments = useSelector(selectComments)
     console.log("comments from state", comments)
-    
+
+    if(comments.length === 0){
     return (
         <View>
             <Text>
@@ -15,4 +17,22 @@ export default function CommentsScreen(){
             </Text>
         </View>
     )
+    } else {
+        return comments.map(comment => {
+            return (
+                <Card
+                    key={comment.id}>
+                    <Card.Title>
+                        {comment.user.fullName}
+                    </Card.Title>
+                    <Card.Divider />
+                    <View>
+                        <Text>
+                            {comment.input}
+                        </Text>
+                    </View>
+                </Card>
+            )
+        })
+    }
 }
